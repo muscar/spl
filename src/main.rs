a135c46017119d8e38319e3caa0a733ae1b508c8
Alex Muscar <alex@muscar.eu>
2021-11-09 11:55:49 +0000

    Hacky blocks & assignments

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,32 @@
 use std::{
+    collections::HashMap,
     io::{self, Write},
     iter::Peekable,
     str::Chars,
 };
 
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+struct Ident(String);
+
+#[derive(Debug)]
+struct TypeName(String);
+
+#[derive(Debug)]
+struct Block {
+    declarations: Vec<(TypeName, Vec<Ident>)>,
+    body: Vec<Stmt>,
+}
+
+#[derive(Debug)]
+enum Stmt {
+    Expr(Box<Expr>),
+    Assign(Box<Expr>, Box<Expr>),
+}
+
 #[derive(Debug)]
 enum Expr {
     IntLit(i64),
+    Ident(Ident),
     BinOp(BinOp, Box<Expr>, Box<Expr>),
 }
 
@@ -19,8 +39,18 @@ enum BinOp {
 #[derive(Debug, PartialEq, Eq)]
 enum Token {
     IntLit,
+    Ident,
     Plus,
     Minus,
+    Becomes,
+    Comma,
+    Period,
+    Semicolon,
+    Colon,
+    KwInteger,
+    KwString,
+    KwBegin,
+    KwEnd,
     Unknown,
     Eof,
 }
@@ -40,7 +70,7 @@ impl<'a> SourceReader<'a> {
         self.chars.peek().copied()
     }
 
-    fn next(&mut self) -> Option<char> {
+    fn read(&mut self) -> Option<char> {
         self.chars.next()
     }
 }
@@ -64,27 +94,59 @@ impl<'a> Iterator for Tokens<'a> {
 
     fn next(&mut self) -> Option<Self::Item> {
         while matches!(self.reader.peek(), Some(c) if c.is_whitespace()) {
-            self.reader.next();
+            self.reader.read();
         }
         self.lexeme.clear();
         match self.reader.peek() {
             Some('+') => {
-                self.lexeme.push(self.reader.next().unwrap());
+                self.lexeme.push(self.reader.read().unwrap());
                 Some((Token::Plus, self.lexeme.clone()))
             }
             Some('-') => {
-                self.lexeme.push(self.reader.next().unwrap());
+                self.lexeme.push(self.reader.read().unwrap());
                 Some((Token::Minus, self.lexeme.clone()))
+            }
+            Some(',') => {
+                self.lexeme.push(self.reader.read().unwrap());
+                Some((Token::Comma, self.lexeme.clone()))
+            }
+            Some('.') => {
+                self.lexeme.push(self.reader.read().unwrap());
+                Some((Token::Period, self.lexeme.clone()))
+            }
+            Some(';') => {
+                self.lexeme.push(self.reader.read().unwrap());
+                Some((Token::Semicolon, self.lexeme.clone()))
+            }
+            Some(':') => {
+                self.lexeme.push(self.reader.read().unwrap());
+                if self.reader.peek() == Some('=') {
+                    self.lexeme.push(self.reader.read().unwrap());
+                    return Some((Token::Becomes, self.lexeme.clone()));
+                }
+                Some((Token::Colon, self.lexeme.clone()))
             }
             Some(c) if c.is_digit(10) => {
                 while matches!(self.reader.peek(), Some(c) if c.is_digit(10)) {
-                    self.lexeme.push(self.reader.next().unwrap());
+                    self.lexeme.push(self.reader.read().unwrap());
                 }
                 Some((Token::IntLit, self.lexeme.clone()))
             }
+            Some(c) if c.is_alphanumeric() => {
+                while matches!(self.reader.peek(), Some(c) if c.is_alphanumeric()) {
+                    self.lexeme.push(self.reader.read().unwrap());
+                }
+                match &self.lexeme[..] {
+                    "integer" => Some((Token::KwInteger, self.lexeme.clone())),
+                    "string" => Some((Token::KwString, self.lexeme.clone())),
+                    "begin" => Some((Token::KwBegin, self.lexeme.clone())),
+                    "end" => Some((Token::KwEnd, self.lexeme.clone())),
+                    _ => Some((Token::Ident, self.lexeme.clone())),
+                }
+            }
             Some(_) => {
                 while matches!(self.reader.peek(), Some(c) if !c.is_whitespace()) {
-                    self.lexeme.push(self.reader.next().unwrap());
+                    self.lexeme.push(self.reader.read().unwrap());
                 }
                 Some((Token::Unknown, self.lexeme.clone()))
             }
@@ -129,14 +191,65 @@ impl<'a> Parser<'a> {
         }
     }
 
-    fn parse(&mut self) -> Expr {
-        self.simple_expr()
+    fn ident(&mut self) -> Ident {
+        Ident(self.expect(Token::Ident))
+    }
+
+    fn ident_list(&mut self) -> Vec<Ident> {
+        let mut idents = vec![self.ident()];
+        while matches!(self.tokens.peek(), Some((Token::Comma, _))) {
+            self.tokens.next();
+            idents.push(self.ident());
+        }
+        idents
+    }
+
+    fn parse(&mut self) -> Block {
+        self.program()
+    }
+
+    fn program(&mut self) -> Block {
+        let expr = self.block();
+        self.expect(Token::Period);
+        expr
+    }
+
+    fn block(&mut self) -> Block {
+        let mut declarations = vec![];
+        self.expect(Token::KwBegin);
+        while matches!(
+            self.tokens.peek(),
+            Some((Token::KwInteger, _)) | Some((Token::KwString, _))
+        ) {
+            let ty_name = TypeName(self.tokens.next().unwrap().1);
+            declarations.push((ty_name, self.ident_list()));
+            self.expect(Token::Semicolon);
+        }
+        let mut body = vec![self.statement()];
+        while matches!(self.tokens.peek(), Some((Token::Semicolon, _))) {
+            self.tokens.next();
+            body.push(self.statement());
+        }
+        self.expect(Token::KwEnd);
+        Block { declarations, body }
+    }
+
+    fn statement(&mut self) -> Stmt {
+        let lhs = self.simple_expr();
+        if matches!(self.tokens.peek(), Some((Token::Becomes, _))) {
+            self.tokens.next();
+            let rhs = self.simple_expr();
+            return Stmt::Assign(Box::new(lhs), Box::new(rhs));
+        }
+        Stmt::Expr(Box::new(lhs))
     }
 
     fn simple_expr(&mut self) -> Expr {
         let mut lhs = self.factor();
-        while matches!(self.tokens.peek(), Some((tok, _)) if *tok == Token::Plus || *tok == Token::Minus)
-        {
+        while matches!(
+            self.tokens.peek(),
+            Some((Token::Plus, _)) | Some((Token::Minus, _))
+        ) {
             let op = match self.tokens.next() {
                 Some((Token::Plus, _)) => BinOp::Plus,
                 Some((Token::Minus, _)) => BinOp::Minus,
@@ -152,39 +265,23 @@ impl<'a> Parser<'a> {
     }
 
     fn factor(&mut self) -> Expr {
-        let lex = self.expect(Token::IntLit);
-        Expr::IntLit(lex.parse().expect("malformed integer literal"))
+        let (tok, lex) = self.tokens.next().expect("expecting factor");
+        match tok {
+            Token::IntLit => Expr::IntLit(lex.parse().expect("malformed integer literal")),
+            Token::Ident => Expr::Ident(Ident(lex.clone())),
+            _ => panic!("sytactic error: expecting factor"),
+        }
     }
 }
 
-// #[derive(Debug)]
-// enum Value {
-//     Int(i64),
-// }
-
-// fn eval(expr: &Expr) -> Value {
-//     match expr {
-//         Expr::IntLit(n) => Value::Int(*n),
-//         Expr::BinOp(BinOp::Plus, lhs, rhs) => {
-//             let lv = eval(lhs);
-//             let rv = eval(rhs);
-//             match (lv, rv) {
-//                 (Value::Int(m), Value::Int(n)) => Value::Int(m + n),
-//             }
-//         }
-//         Expr::BinOp(BinOp::Minus, lhs, rhs) => {
-//             let lv = eval(lhs);
-//             let rv = eval(rhs);
-//             match (lv, rv) {
-//                 (Value::Int(m), Value::Int(n)) => Value::Int(m - n),
-//             }
-//         }
-//     }
-// }
+#[derive(Debug, Clone, Copy)]
+struct LocalIdx(usize);
 
 #[derive(Debug)]
 enum OpCode {
     LdcI8(i64),
+    LdLoc(LocalIdx),
+    StLoc(LocalIdx),
     Add,
     Sub,
     Ret,
@@ -192,11 +289,21 @@ enum OpCode {
 
 struct CodeGen {
     code: Vec<OpCode>,
+    next_local_idx: usize,
 }
 
 impl CodeGen {
     fn new() -> Self {
-        Self { code: Vec::new() }
+        Self {
+            code: Vec::new(),
+            next_local_idx: 0,
+        }
+    }
+
+    fn add_local(&mut self) -> LocalIdx {
+        let idx = LocalIdx(self.next_local_idx);
+        self.next_local_idx += 1;
+        idx
     }
 
     fn emit(&mut self, op: OpCode) {
@@ -204,19 +311,44 @@ impl CodeGen {
     }
 }
 
+struct ActivationRecord {
+    locals: Vec<i64>,
+}
+
+impl ActivationRecord {
+    fn new(locals_count: usize) -> Self {
+        Self {
+            locals: vec![0; locals_count],
+        }
+    }
+}
+
 struct Vm {
     stack: Vec<i64>,
+    call_stack: Vec<ActivationRecord>,
 }
 
 impl Vm {
     fn new() -> Self {
-        Self { stack: Vec::new() }
+        Self {
+            stack: Vec::new(),
+            call_stack: Vec::new(),
+        }
     }
 
-    fn run(&mut self, code: &[OpCode]) -> i64 {
-        for op in code {
+    fn run(&mut self, gen: &CodeGen) -> i64 {
+        self.call_stack
+            .push(ActivationRecord::new(gen.next_local_idx));
+        for op in &gen.code {
             match op {
                 OpCode::LdcI8(n) => self.stack.push(*n),
+                OpCode::LdLoc(idx) => self
+                    .stack
+                    .push(self.call_stack.last().unwrap().locals[idx.0]),
+                OpCode::StLoc(idx) => {
+                    let arec = self.call_stack.last_mut().unwrap();
+                    arec.locals[idx.0] = self.stack.pop().expect("stack underflow");
+                }
                 OpCode::Add => {
                     let n = self.stack.pop().expect("stack underflow");
                     let m = self.stack.pop().expect("stack underflow");
@@ -230,21 +362,59 @@ impl Vm {
                 OpCode::Ret => break,
             }
         }
+        self.call_stack.pop();
         self.stack.pop().expect("stack underflow")
     }
 }
 
-fn compile(expr: &Expr, gen: &mut CodeGen) {
+fn compile_block(block: &Block, gen: &mut CodeGen) {
+    let mut names = HashMap::new();
+    for (_, idx) in &block.declarations {
+        for id in idx {
+            let idx = gen.add_local();
+            names.insert(id.clone(), idx);
+            // gen.emit(OpCode::LdcI8(0));
+            // gen.emit(OpCode::StLoc(idx));
+        }
+    }
+    for s in &block.body {
+        compile_stmt(s, gen, &mut names);
+    }
+}
+
+fn compile_stmt(stmt: &Stmt, gen: &mut CodeGen, names: &mut HashMap<Ident, LocalIdx>) {
+    match stmt {
+        Stmt::Expr(e) => compile_expr(e, gen, names),
+        Stmt::Assign(lhs, rhs) => match &**lhs {
+            Expr::Ident(id) => {
+                let idx = names
+                    .get(&id)
+                    .expect(&format!("unbound identifier: {}", id.0));
+                compile_expr(rhs, gen, names);
+                gen.emit(OpCode::StLoc(*idx))
+            }
+            _ => panic!("general assignment is not implemented"),
+        },
+    }
+}
+
+fn compile_expr(expr: &Expr, gen: &mut CodeGen, names: &HashMap<Ident, LocalIdx>) {
     match expr {
         Expr::IntLit(n) => gen.emit(OpCode::LdcI8(*n)),
+        Expr::Ident(id) => {
+            let idx = names
+                .get(id)
+                .expect(&format!("unbound identifier: {}", id.0));
+            gen.emit(OpCode::LdLoc(*idx));
+        }
         Expr::BinOp(BinOp::Plus, lhs, rhs) => {
-            compile(lhs, gen);
-            compile(rhs, gen);
+            compile_expr(lhs, gen, names);
+            compile_expr(rhs, gen, names);
             gen.emit(OpCode::Add);
         }
         Expr::BinOp(BinOp::Minus, lhs, rhs) => {
-            compile(lhs, gen);
-            compile(rhs, gen);
+            compile_expr(lhs, gen, names);
+            compile_expr(rhs, gen, names);
             gen.emit(OpCode::Sub);
         }
     }
@@ -262,13 +432,12 @@ fn main() {
         let mut s = Scanner::new(sr);
         let mut p = Parser::new(s.scan());
         let node = p.parse();
-        // println!("; {:?}", node);
-        // println!("{:?} ; eval", eval(&node));
+        println!("; {:?}", node);
 
         let mut gen = CodeGen::new();
-        compile(&node, &mut gen);
+        compile_block(&node, &mut gen);
         let mut vm = Vm::new();
-        println!("; {:?} ; compile", vm.run(&gen.code));
+        println!("; {:?} ; compile", vm.run(&gen));
 
         buf.clear();
     }
